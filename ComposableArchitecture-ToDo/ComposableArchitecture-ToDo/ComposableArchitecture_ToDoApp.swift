f81a48a32bc68ca15d605bffbaea4e8bbe544ffe
Luis Ramirez <lramirezsuarez@gmail.com>
2022-08-09 14:07:56 -0500

    First commit with empty project.

---
+++ ComposableArchitecture-ToDo/ComposableArchitecture-ToDo/ComposableArchitecture_ToDoApp.swift
@@ -0,0 +1,21 @@
+//
+//  ComposableArchitecture_ToDoApp.swift
+//  ComposableArchitecture-ToDo
+//
+//  Created by Luis Alejandro Ramirez Suarez on 9/08/22.
+//
+
+import SwiftUI
+
+struct AppState {
+    
+}
+
+@main
+struct ComposableArchitecture_ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComposableArchitecture-ToDo/ComposableArchitecture-ToDo/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ComposableArchitecture-ToDo
+//
+//  Created by Luis Alejandro Ramirez Suarez on 9/08/22.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
