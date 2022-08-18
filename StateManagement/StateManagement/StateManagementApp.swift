44144753168caa8aa407be0d7aab5e80a7039d3d
Luis Ramirez <lramirezsuarez@gmail.com>
2022-08-18 14:29:04 -0500

    State Management Part 1.

---
+++ StateManagement/StateManagement/StateManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateManagementApp.swift
+//  StateManagement
+//
+//  Created by Luis Alejandro Ramirez Suarez on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct StateManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
